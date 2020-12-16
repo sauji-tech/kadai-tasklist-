72de3e7408e1ee59aa0bf1aeee7ae25aabf5ddaf
c.i <sauji355@gmail.com>
2020-12-16 09:45:49 +0000

    login in tasklist

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user, optional: true 
 
   validates :status, presence: true, length: { maximum: 10 }
   validates :content, presence: true, length: { maximum: 255 }
